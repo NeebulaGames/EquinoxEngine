91c0486f4e44941e919f029e633b90e78cfbbed5
Pol Vilà <polvilasalo@gmail.com>
2017-02-03 18:39:53 +0100

    Delete stopwatch functionalities

--- Equinox/SimpleTimer.h
+++ Equinox/SimpleTimer.h
@@ -28,33 +28,9 @@ public:
 		return pausedTicks;
 	}
 
-	void Resume()
-	{
-		if(!running)
-		{
-			running = true;
-			runTicks = SDL_GetTicks() - pausedTicks;
-			pausedTicks = 0;
-		}
-	}
-
-	void Clear()
-	{
-		running = false;
-		runTicks = 0;
-		pausedTicks = 0;
-	}
-
 	unsigned int Read() const
 	{
-		if (running)
-			return SDL_GetTicks() - runTicks;
 		return pausedTicks;
-	}
-
-	bool IsRunning() const
-	{
-		return running;
 	}
 
 private:
