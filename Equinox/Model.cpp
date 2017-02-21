ca18034d82fd2d3a7afefb6821dd3f49c0ccf740
Joel Lopez <jowie.jowie@gmail.com>
2017-02-22 00:33:14 +0100

    Cleanup model class

--- Equinox/Model.cpp
+++ Equinox/Model.cpp
@@ -18,10 +18,11 @@ Model::~Model()
 
 void Model::Load(const char* path, const char* file)
 {
-	std::string filePath(path);
-	filePath.append(file);
+	LOG("Loading model %s", file);
+	char filePath[256];
+	sprintf_s(filePath, "%s%s", path, file);
 
-	scene = aiImportFile(filePath.data(), aiProcess_PreTransformVertices | aiProcess_FlipUVs);
+	scene = aiImportFile(filePath, aiProcess_PreTransformVertices | aiProcess_FlipUVs);
 
 	indexes = new Uint32*[scene->mNumMeshes];
 
@@ -42,31 +43,21 @@ void Model::Load(const char* path, const char* file)
 	}
 
 	materials = new GLuint[scene->mNumMaterials];
-	int numTexturesByMaterial;
+	
 	for(int i = 0; i < scene->mNumMaterials; ++i)
 	{
-		numTexturesByMaterial = scene->mMaterials[i]->GetTextureCount(aiTextureType_DIFFUSE);
-		//materials[i] = new GLuint[numTexturesByMaterial];
+		int numTexturesByMaterial = scene->mMaterials[i]->GetTextureCount(aiTextureType_DIFFUSE);
 		if (numTexturesByMaterial > 0)
 		{
 			aiMaterial* material = scene->mMaterials[i];
 
 			aiString fileName;
-			aiReturn ret = material->GetTexture(aiTextureType_DIFFUSE, 0, &fileName);
-			filePath = path;
-			filePath.append(fileName.C_Str());
-			LOG("PATH: %s", filePath.data());
+			material->GetTexture(aiTextureType_DIFFUSE, 0, &fileName);
+			
+			sprintf_s(filePath, "%s%s", path, fileName.C_Str());
 
-			GLuint matNum = 0;
-			char tmp[512];
-			strcpy_s(tmp, filePath.c_str());
-
-			matNum = App->textures->Load(filePath.c_str());
-
-
-
-			LOG("Texture ID: %i", matNum);
-			//assert(matNum != 0);
+			GLuint matNum = App->textures->Load(filePath);
+			assert(matNum != 0);
 
 			materials[i] = matNum;
 		}
@@ -74,7 +65,7 @@ void Model::Load(const char* path, const char* file)
 			materials[i] = 0;
 	}
 
-	LOG("FI LOAD");
+	LOG("Loaded %i meshes and %i materials", scene->mNumMeshes, scene->mNumMaterials);
 }
 
 bool Model::CleanUp()
@@ -87,45 +78,6 @@ bool Model::CleanUp()
 	return true;
 }
 
-//void Model::Draw()
-//{
-//	glPushMatrix();
-//	glBegin(GL_TRIANGLES);
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//	glEnableClientState(GL_NORMAL_ARRAY);
-//	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-//
-//	for (int i = 0; i < 1; ++i)
-//	{
-//		aiMesh* mesh = scene->mMeshes[i];
-//
-//		glBindTexture(GL_TEXTURE_2D, materials[mesh->mMaterialIndex]);
-//
-//		//glVertexPointer(3, GL_FLOAT, 0, &mesh->mVertices[0]); 
-//		/*glNormalPointer(GL_FLOAT, 0, &mesh->mNormals[0]);
-//		glTexCoordPointer(2, GL_FLOAT, 0, &mesh->mTextureCoords[0]);*/
-//		//glDrawElements(GL_TRIANGLES, mesh->mNumFaces, GL_UNSIGNED_INT, indexes[i]); 
-//
-//		for (int iFace = 0; iFace < mesh->mNumFaces; ++iFace)
-//		{
-//			aiFace* face = &mesh->mFaces[iFace];
-//			Uint32* indexes = face->mIndices;
-//
-//			if (mesh->HasTextureCoords(0))
-//			{
-//				glTexCoord2fv(mesh->mTextureCoords[0])
-//			}
-//
-//			glVertex3fv(&mesh->mVertices[*indexes++][0]);
-//			glVertex3fv(&mesh->mVertices[*indexes++][0]);
-//			glVertex3fv(&mesh->mVertices[*indexes][0]);
-//		}
-//	}
-//
-//	glEnd();
-//	glPopMatrix();
-//}
-
 void Model::Draw()
 {
 	glPushMatrix();
@@ -134,15 +86,11 @@ void Model::Draw()
 	glEnableClientState(GL_NORMAL_ARRAY);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-	int numTexturesByMaterial;
 	for (unsigned i = 0; i < scene->mNumMeshes; ++i)
 	{
 		aiMesh* mesh = scene->mMeshes[i];
-
-		numTexturesByMaterial = scene->mMaterials[mesh->mMaterialIndex]->GetTextureCount(aiTextureType_DIFFUSE);
-		//glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-		//for (int y = 0; y < numTexturesByMaterial; ++y)
-			glBindTexture(GL_TEXTURE_2D, materials[mesh->mMaterialIndex]);
+		
+		glBindTexture(GL_TEXTURE_2D, materials[mesh->mMaterialIndex]);
 
 		glVertexPointer(3, GL_FLOAT, sizeof(aiVector3D), &mesh->mVertices[0]);
 		glNormalPointer(GL_FLOAT, sizeof(aiVector3D), &mesh->mNormals[0]);
@@ -152,9 +100,9 @@ void Model::Draw()
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
 	glPopMatrix();
 }
