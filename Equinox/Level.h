6a33307eec3170bfd1823328e8c2475160308112
Joel Lopez <jowie.jowie@gmail.com>
2017-03-02 21:35:48 +0100

    Load and draw scenes

--- Equinox/Level.h
+++ Equinox/Level.h
@@ -1,11 +1,10 @@
 ﻿#ifndef __LEVEL_H__
 #define __LEVEL_H__
 
-#include "Primitive.h"
 #include <assimp/vector3.h>
-#include <assimp/quaternion.h>
 #include <assimp/color4.h>
 #include <assimp/scene.h>
+#include "Primitive.h"
 
 class Level : public Primitive
 {
@@ -17,7 +16,8 @@ public:
 	{
 		std::string name;
 		aiVector3D position = aiVector3D(0, 0, 0);
-		aiQuaternion rotation = aiQuaternion(1, 0, 0, 0);
+		aiVector3D scale = aiVector3D(1, 1, 1);
+		Quat rotation = Quat(0, 0, 0, 1);
 		std::vector<unsigned> meshes;
 		Node* parent = nullptr;
 		std::vector<Node*> childs;
@@ -26,10 +26,11 @@ public:
 	struct Mesh
 	{
 		unsigned material = 0;
-		GLuint* _vertexIDs = nullptr;
-		GLuint* _normalIDs = nullptr;
-		GLuint* _textureIDs = nullptr;
-		GLuint* _indexesID = nullptr;
+		GLuint vertexID = 0;
+		GLuint normalID = 0;
+		GLuint textureID = 0;
+		aiVector3D** textureCoords;
+		GLuint indexesID = 0;
 		unsigned num_vertices = 0;
 		unsigned num_indices = 0;
 	};
@@ -39,13 +40,13 @@ public:
 		aiColor4D ambient = aiColor4D(1.0f, 1.0f, 1.0f, 1.0f);
 		aiColor4D diffuse = aiColor4D(1.0f, 1.0f, 1.0f, 1.0f);
 		aiColor4D specular = aiColor4D(0.0f, 0.0f, 0.0f, 0.0f);
-		float shiness = 0.0f;
+		float shininess = 0.0f;
 		unsigned texture = 0;
 	};
 
 	Node* root = nullptr;
-	std::vector<Mesh> meshes;
-	std::vector<Material> materials;
+	std::vector<Mesh*> meshes;
+	std::vector<Material*> materials;
 
 	void Load(const char* path, const char* file);
 	bool CleanUp() override;
@@ -54,11 +55,11 @@ public:
 	unsigned GetNumMeshes() const { return meshes.size(); }
 	unsigned GetNumMaterials() const { return materials.size(); }
 
-	Mesh& GetMesh(unsigned index) { return meshes[index]; }
-	const Mesh& GetMesh(unsigned index) const { return meshes[index]; }
+	Mesh* GetMesh(unsigned index) { return meshes[index]; }
+	const Mesh* GetMesh(unsigned index) const { return meshes[index]; }
 
-	Material& GetMaterial(unsigned index) { return materials[index]; }
-	const Material& GetMaterial(unsigned index) const { return materials[index]; }
+	Material* GetMaterial(unsigned index) { return materials[index]; }
+	const Material* GetMaterial(unsigned index) const { return materials[index]; }
 
 	Node* GetRootNode() { return root; }
 	const Node* GetRootNode() const { return root; }
@@ -67,9 +68,11 @@ public:
 	void LinkNode(Node* node, Node* destination);
 
 private:
-	const aiScene* scene = nullptr;
+	void loadNodes(aiNode* originalNode, Node* node);
 
-	
+	void loadMeshes(const aiScene* scene, const char* path);
+
+	void drawNode(const Node* node);
 };
 
 #endif // __LEVEL_H__
