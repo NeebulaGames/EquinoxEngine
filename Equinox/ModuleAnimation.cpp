ef239f9a50ff786d28e228c03b6b8647de19c54e
Pol Vilà <polvilasalo@gmail.com>
2017-03-25 21:02:30 +0100

    Add Play and Stop methods

--- Equinox/ModuleAnimation.cpp
+++ Equinox/ModuleAnimation.cpp
@@ -3,6 +3,7 @@
 #include <assimp/postprocess.h>
 #include "ModuleAnimation.h"
 #include <MathGeoLib/include/Math/Quat.h>
+#include "Engine.h"
 
 ModuleAnimation::ModuleAnimation(bool start_enabled) : Module(start_enabled)
 {
@@ -14,19 +15,25 @@ ModuleAnimation::~ModuleAnimation()
 
 bool ModuleAnimation::CleanUp()
 {
-	AnimationsMap::iterator it = _animations.begin();
-	for (std::pair<std::string, Animation*> element : _animations)
+	AnimMap::iterator it = _animations.begin();
+	for (std::pair<std::string, Anim*> element : _animations)
 	{
-		for (Channel* channel : element.second->Channels)
-		{
+		for (NodeAnim* channel : element.second->Channels)
 			RELEASE(channel);
-		}
+
 		RELEASE(element.second);
 	}
 
 	_animations.clear();
 
 	return true;
+}
+
+update_status ModuleAnimation::Update()
+{
+	for (AnimInstance* animInstance : _instances)
+		if(animInstance != nullptr)
+			animInstance->time += App->DeltaTime;
 }
 
 void ModuleAnimation::Load(const char* name, const char* file)
@@ -39,15 +46,15 @@ void ModuleAnimation::Load(const char* name, const char* file)
 
 	aiAnimation** animations = scene->mAnimations;
 
-	Animation* anim = new Animation();
+	Anim* anim = new Anim();
 	anim->Duration = animations[0]->mDuration;
-	anim->Channels = std::vector<Channel*>(animations[0]->mNumChannels);
+	anim->Channels = std::vector<NodeAnim*>(animations[0]->mNumChannels);
 
 	for (unsigned int i = 0; i < animations[0]->mNumChannels; ++i)
 	{
 		aiNodeAnim* aiNodeAnim = animations[0]->mChannels[i];
 		
-		anim->Channels[i] = new Channel();
+		anim->Channels[i] = new NodeAnim();
 		anim->Channels[i]->NodeName = aiNodeAnim->mNodeName.C_Str();
 
 		for(unsigned int j = 0; j < aiNodeAnim->mNumPositionKeys; ++j)
@@ -65,4 +72,41 @@ void ModuleAnimation::Load(const char* name, const char* file)
 
 	_animations[name] = anim;
 	aiReleaseImport(scene);
+}
+
+ModuleAnimation::AnimInstanceID ModuleAnimation::Play(const char* name)
+{
+	AnimInstance* animInstance = new AnimInstance();
+	animInstance->anim = _animations[name];
+	unsigned pos;
+
+	if(_holes.empty())
+	{
+		_instances.push_back(animInstance);
+		pos = _instances.size() - 1;
+	}
+	else
+	{
+		pos = _holes.at(_holes.size() - 1);
+		_instances.at(pos) = animInstance;
+		_holes.pop_back();
+	}
+
+	return pos;
+}
+
+
+void ModuleAnimation::Stop(AnimInstanceID id)
+{
+	_holes.push_back(id);
+	RELEASE(_instances[id]);
+}
+
+bool ModuleAnimation::GetTransform(AnimInstanceID instance, const char* channelName, aiVector3D& position, aiQuaternion& rotation) const
+{
+	for (NodeAnim* channel : _instances[instance]->anim->Channels)
+	{
+		if (channel->NodeName == channelName)
+			channel->Positions;//Continue
+	}
 }
--- Equinox/ModuleAnimation.h
+++ Equinox/ModuleAnimation.h
@@ -5,33 +5,57 @@
 #include <map>
 #include <MathGeoLib/include/Math/float3.h>
 
-struct Channel
+struct NodeAnim
 {
 	std::string NodeName;
 	std::vector<float3*> Positions;
 	std::vector<Quat*> Rotations;
 };
 
-struct Animation
+struct Anim
 {
 	double Duration = 0.f;
-	std::vector<Channel*> Channels;
+	std::vector<NodeAnim*> Channels;
 };
+
+struct AnimInstance
+{
+	Anim* anim;
+	unsigned time = 0;
+	bool loop = true;
+	
+	AnimInstance* next = nullptr;
+	unsigned blend_duration = 0;
+	unsigned blend_time = 0;
+};
+
 
 class ModuleAnimation : public Module
 {
 public:
+	typedef unsigned AnimInstanceID;
+
 	ModuleAnimation(bool start_enabled = true);
 	~ModuleAnimation();
 
 	bool CleanUp() override;
+	update_status Update() override;
 
 	void Load(const char* name, const char* file);
 
-private:
-	typedef std::map<std::string, Animation*> AnimationsMap;
+	AnimInstanceID Play(const char* name);
+	void Stop(AnimInstanceID id);
 
-	AnimationsMap _animations;
+	bool GetTransform(AnimInstanceID instance, const char* channelName, aiVector3D& position, aiQuaternion& rotation) const;
+
+private:
+	typedef std::map<std::string, Anim*> AnimMap;
+	typedef std::vector<AnimInstance*> InstanceList;
+	typedef std::vector<AnimInstanceID> HoleList;
+
+	AnimMap _animations;
+	InstanceList _instances;
+	HoleList _holes;
 };
 
 #endif // __MODULEANIMATION_H__
