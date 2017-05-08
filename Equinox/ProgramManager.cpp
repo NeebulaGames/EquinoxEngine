a64a76ec4be9122d6d263507499c556d3e1baf07
Ennio Casas Puglielli <theepsi@gmail.com>
2017-05-08 20:41:59 +0200

    Refactor Load method

--- Equinox/ProgramManager.cpp
+++ Equinox/ProgramManager.cpp
@@ -22,7 +22,7 @@ bool ProgramManager::CleanUp()
 	return true;
 }
 
-void ProgramManager::Load(std::string name, char* filepath, GLenum shaderType)
+void ProgramManager::Load(const std::string &name, const char* filepath, const GLenum shaderType)
 {
 	if (programs.find(name) == programs.end())
 	{
@@ -35,25 +35,75 @@ void ProgramManager::Load(std::string name, char* filepath, GLenum shaderType)
 		fileSize = ftell(shaderFile);
 		rewind(shaderFile);
 
-		shaderSource = (char*)malloc(sizeof(char) * (fileSize + 1));
+		//move to new
+		shaderSource = new char[fileSize + 1];
 		fread(shaderSource, sizeof(char), fileSize, shaderFile);
 		shaderSource[fileSize] = '\0';
 		fclose(shaderFile);
 
 		unsigned int shader = glCreateShader(shaderType);
-		glShaderSource(shader, 1, (const GLchar**)&shaderSource, NULL);
+		glShaderSource(shader, 1, const_cast<const GLchar**>(&shaderSource), NULL);
 
 		glCompileShader(shader);
+
+		logShaderCompiler(shader);
 
 		unsigned int shaderProgram = glCreateProgram();
 		glAttachShader(shaderProgram, shader);
 		glLinkProgram(shaderProgram);
 
-		programs.insert(std::pair<std::string, GLint>(name, shaderProgram));
+		logProgramLinker(shaderProgram);
+
+		delete[] shaderSource;
+
+		programs.insert(std::pair<std::string, GLuint>(name, shaderProgram));
 
 	}
 	else
 	{
 		//TODO: show some kind of error because program with that name already exists.
 	}
+}
+
+GLuint ProgramManager::GetProgramByName(const std::string &name) const
+{
+	auto it = programs.find(name);
+	return it != programs.end() ? it->second : 0;
+}
+
+void ProgramManager::UseProgram(const std::string &name) const
+{
+	glUseProgram(GetProgramByName(name));
+}
+
+void ProgramManager::logShaderCompiler(const GLuint shader) const
+{
+	int log_size;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
+
+	char* compiler_log = new char[log_size + 1];
+	int logger_size;
+	glGetShaderInfoLog(shader, log_size + 1, &logger_size, reinterpret_cast<GLchar*>(&compiler_log));
+
+	compiler_log[logger_size] = '\0';
+
+	LOG("SHADER COMPILER LOG START");
+	LOG(compiler_log);
+	LOG("SHADER COMPILER LOG END");
+}
+
+void ProgramManager::logProgramLinker(const GLuint program) const
+{
+	int log_size;
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_size);
+
+	char* compiler_log = new char[log_size + 1];
+	int logger_size;
+	glGetProgramInfoLog(program, log_size + 1, &logger_size, reinterpret_cast<GLchar*>(&compiler_log));
+
+	compiler_log[logger_size] = '\0';
+
+	LOG("PROGRAM LINKER LOG START");
+	LOG(compiler_log);
+	LOG("PROGRAM LINKER LOG END");
 }
--- Equinox/ProgramManager.h
+++ Equinox/ProgramManager.h
@@ -13,10 +13,16 @@ public:
 	bool Init() override;
 	bool CleanUp() override;
 
-	void Load(std::string name, char* filepath, GLenum shaderType);
+	void Load(const std::string &name, const char* filepath, const GLenum shaderType);
+
+	GLuint GetProgramByName(const std::string &name) const;
+	void UseProgram(const std::string &name) const;
 
 private:
 	std::map<std::string, GLuint> programs;
+
+	void logShaderCompiler(const GLuint shader) const;
+	void logProgramLinker(const GLuint program) const;
 };
 
 #endif
