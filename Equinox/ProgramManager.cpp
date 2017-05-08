99911282a73a55ba34f18d5ba097e749966d8580
Ennio Casas Puglielli <theepsi@gmail.com>
2017-05-08 17:40:34 +0200

    Add simple ProgramManager logic

---
+++ Equinox/ProgramManager.cpp
@@ -0,0 +1,59 @@
+﻿#include "Globals.h"
+#include "Engine.h"
+#include "ProgramManager.h"
+
+ProgramManager::ProgramManager()
+{
+
+}
+
+ProgramManager::~ProgramManager()
+{
+
+}
+
+bool ProgramManager::Init()
+{
+	return true;
+}
+
+bool ProgramManager::CleanUp()
+{
+	return true;
+}
+
+void ProgramManager::Load(std::string name, char* filepath, GLenum shaderType)
+{
+	if (programs.find(name) == programs.end())
+	{
+		FILE* shaderFile = fopen(filepath, "r");
+		int fileSize = 0;
+
+		char* shaderSource;
+
+		fseek(shaderFile, 0, SEEK_END);
+		fileSize = ftell(shaderFile);
+		rewind(shaderFile);
+
+		shaderSource = (char*)malloc(sizeof(char) * (fileSize + 1));
+		fread(shaderSource, sizeof(char), fileSize, shaderFile);
+		shaderSource[fileSize] = '\0';
+		fclose(shaderFile);
+
+		unsigned int shader = glCreateShader(shaderType);
+		glShaderSource(shader, 1, (const GLchar**)&shaderSource, NULL);
+
+		glCompileShader(shader);
+
+		unsigned int shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, shader);
+		glLinkProgram(shaderProgram);
+
+		programs.insert(std::pair<std::string, GLint>(name, shaderProgram));
+
+	}
+	else
+	{
+		//TODO: show some kind of error because program with that name already exists.
+	}
+}
---
+++ Equinox/ProgramManager.h
@@ -0,0 +1,23 @@
+﻿#ifndef __PROGRAM_MANAGER__H__
+#define __PROGRAM_MANAGER__H__
+
+#include "Module.h"
+#include <map>
+
+class ProgramManager : public Module
+{
+public:
+	ProgramManager();
+	~ProgramManager();
+
+	bool Init() override;
+	bool CleanUp() override;
+
+	void Load(std::string name, char* filepath, GLenum shaderType);
+
+private:
+	std::map<std::string, GLuint> programs;
+};
+
+#endif
+
